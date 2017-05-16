a2eefd9803da5d6a511e6a45d1f656207378ac39
romoreira <romoreira@NTB-TEL114387.network.ctbc>
2017-05-16 15:08:23 -0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,27 @@ import java.io.PrintStream;
 public class Main {
 	public static void main(String args[]){
 		try{
-			Process p = Runtime.getRuntime().exec("ssh 172.24.158.210");
+			Process p = Runtime.getRuntime().exec("ssh 172.24.149.100 -l admin");
 			PrintStream out = new PrintStream(p.getOutputStream());
 			BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
-			out.println("root");
+			
+			Thread.sleep(4000);
+			System.out.println("Antes de exibir pela primeira vez");
 			while (in.ready()) {
-			  String s = in.readLine();
-			  System.out.println(s);
+				String s = in.readLine();
+				System.out.println(s);
 			}
-			out.println("admin");
-
+			out.println("zhone");
+			Thread.sleep(4000);
+			
+			System.out.println("Antes de exibir novamente");
 			while (in.ready()) {
 				String s = in.readLine();
 				System.out.println(s);
 			}
 			
+			System.out.println("Antes do waitfor");
 			p.waitFor();
 		}catch(Exception e){
 			System.out.println("Erro: "+e);

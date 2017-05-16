40abab92e0265b17752e675450788aed9dd68712
romoreira <romoreira@NTB-TEL114387.network.ctbc>
2017-05-16 14:21:50 -0300

    SSH baseado em Processo

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.net.Socket;
 
 public class Main {
 	public static void main(String args[]){

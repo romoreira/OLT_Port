acdb8f6bcd72ffba6782928247da3a486c452eef
romoreira <romoreira@NTB-TEL114387.network.ctbc>
2017-05-17 16:02:42 -0300

    Funcionando quando não tem limite de logins

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 import org.apache.commons.net.telnet.TelnetClient;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 
 public class Main {
 	
@@ -10,6 +18,8 @@ public class Main {
     private PrintStream out;
     private String prompt = "%";
 
+    public Main(){}
+    
     public Main(String server, String user, String password) {
         try {
             // Connect to the specified server
@@ -121,12 +131,17 @@ public class Main {
             						if (sb.toString().endsWith(pattern)) {
             							return sb.toString();
             						}
-            					}
+               					}
             					else{
             						if(sb.toString().contains("break")){
             							try {
-            					            this.write("a");
-            					            Thread.sleep(4000);
+            								if(sb.toString().contains("Unknown")){
+            									System.out.println("Finalizou: "+comando);
+            									return sb.toString();
+            								}
+            								else{
+            									sb.append(sendCommand(comando));
+            								}
             					        } catch (Exception e) {
             					            System.out.println("Erro: "+e.getMessage());
             					        }
@@ -143,7 +158,11 @@ public class Main {
     	}
     	return null;
     }
-    	
+    
+    public void flush(){
+    	out.flush();
+    }
+    
     public void write(String value) {
         try {
             out.println(value);
@@ -157,6 +176,7 @@ public class Main {
     public String sendCommand(String command) {
         try {
             write(command);
+            System.out.println("Executando o comando: "+command);
             Thread.sleep(10000);
             return readUntil(">", command);
         } catch (Exception e) {
@@ -173,21 +193,113 @@ public class Main {
         }
     }
 	
-	public static void main(String args[]){
-		//a-gn-ol-iai-001-salseiros-01
-		//172.24.158.194
-		
-		//a-gn-ol-lgs-001-centro-01
-		//172.24.163.34
-		
+    public void criaArquivoTemporario(String saidaTelnet, String nomeOLT, String sfp){
+		FileWriter arquivo;
 		try {
-            Main telnet = new Main("172.30.17.253", "root", "admin");
-            telnet.sendCommand("scroll 512");
-            telnet.sendCommand("display board 0/0");
-            telnet.disconnect();
-            System.out.println("DONE");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+			arquivo = new FileWriter(new File(nomeOLT+"_SFP"+sfp+"_huawei_concessao.tmp"));
+			arquivo.write(saidaTelnet);
+			arquivo.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Erro ao criar arquivo temporario!"+e.getMessage());
+		}
+    }
+    
+    public String trataArquivoTemporario(String nomeOLT, String sfp){
+    	try{
+    		FileReader fileReader = new FileReader(nomeOLT+"_SFP"+sfp+"_huawei_concessao.tmp");
+    			BufferedReader reader = new BufferedReader(fileReader);
+    			String linha = null;
+    			StringBuffer sb = new StringBuffer();
+    			while((linha = reader.readLine()) != null){
+    				if(linha.contains("In port")){
+    					sb.append(linha).append("\n");
+    				}
+    			}
+    			fileReader.close();
+    			reader.close();
+    			return sb.toString();
+    	}
+    	catch(Exception e){
+    		System.out.println("Erro ao tratar arquivo temporário - talvez ele não existe!\n"+ e.getMessage());
+    	}
+		return "Erro - tratamento de arquivo";
+    }
+    
+    public void criaArquivoFinal(String saidaTelnetFinal, String nomeOLT, String sfp){
+    	FileWriter arquivo;
+		try {
+			arquivo = new FileWriter(new File(nomeOLT+"_SFP"+sfp+"_huawei_concessao.txt"));
+			arquivo.write(saidaTelnetFinal);
+			arquivo.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Erro ao criar arquivo temporario!"+e.getMessage());
+		}
+    }
+    
+	public static void main(String args[]){
+	
+//		for (int i = 0; i < args.length; i++){
+//            if(args[i] == null){
+//            	System.out.println("Erro com os parametros");
+//            }
+//		}
+
+		String nomeArquivoOLTs = "olts_concessao_huawei";
+		ArrayList<Elemento> listaOLTs = new ArrayList<Elemento>();
+		
+		
+		try{
+    		FileReader fileReader = new FileReader(nomeArquivoOLTs+".txt");
+    			BufferedReader reader = new BufferedReader(fileReader);
+    			String linha = null;
+    			StringBuffer sb = new StringBuffer();
+    			while((linha = reader.readLine()) != null){
+    				
+    				Elemento olt = new Elemento();
+    				
+    				String nomeOLT = linha.substring(0, linha.indexOf(";"));
+    				//System.out.println("Nome OLT: "+nomeOLT);
+    				olt.setNomeOLT(nomeOLT);
+    				
+    				String temporaria = linha.substring(linha.indexOf(";") + 1,linha.length());
+    				//System.out.println("String temporaria: "+temporaria);
+    				String ipOLT = temporaria.substring(0, temporaria.indexOf(";"));
+    				olt.setIpOLT(ipOLT);
+    				//System.out.println("IP da OLT: "+ipOLT);
+    				
+    				olt.setQtdSFP(temporaria.substring(temporaria.indexOf(";")+1));
+    				
+    				listaOLTs.add(olt);
+    				
+    			}
+    			fileReader.close();
+    			reader.close();
+    	}
+    	catch(Exception e){
+    		System.out.println("Erro ao ler arquivo (Parametro Inicial)"+e.getMessage());
+    	}
+		
+		for (int i = 0; i < listaOLTs.size(); i++) {
+			int qtdSFP = 0;
+			qtdSFP = Integer.parseInt(listaOLTs.get(i).getQtdSFP());
+			for (int j = 0; j < qtdSFP; j++) {
+				try {
+					Main telnet = new Main(listaOLTs.get(i).getIpOLT(), "root", "admin");
+					telnet.sendCommand("scroll 512");
+					telnet.criaArquivoTemporario(telnet.sendCommand("display board 0/" + j),listaOLTs.get(i).getNomeOLT(), String.valueOf(j));
+					telnet.criaArquivoFinal(telnet.trataArquivoTemporario(listaOLTs.get(i).getNomeOLT(), String.valueOf(j)),listaOLTs.get(i).getNomeOLT(), String.valueOf(j));
+					telnet.disconnect();
+					System.out.println("DONE");
+					Thread.sleep(10000);
+				} catch (Exception e) {
+					System.out.println("Erro no loop principal");
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
